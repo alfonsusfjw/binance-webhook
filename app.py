dbb7c08612f1bd48f0441e905020ee2702074ad6
alfonsusfjw <114491399+alfonsusfjw@users.noreply.github.com>
2022-10-03 00:34:12 +0700

    main

--- app.py
+++ app.py
@@ -14,24 +14,24 @@ eBaseURL=config.testnet.url
 um_futures_client = UMFutures(key=eKey, secret=eSecret, base_url=eBaseURL)
 
 # Database
-eSymbol = str("").upper() # BTCUSDT, BNBUSDT, ETHUSDT
-eSide = str("").upper() # BUY, SELL
-eType = str("MARKET").upper() # LIMIT, MARKET, STOP, STOP_MARKET, TAKE_PROFIT
-ePositionSide = str("BOTH").upper() # BOTH, LONG, SHORT
-eTimeInForce = str("GTC").upper()
-eQuantity = float()
-eReduceOnly = str("").upper()
-ePrice = float()
-eNewClientOrderId = "byTradingTool" # Optional string. An unique ID among open orders. Automatically generated if not sent.
-eStopPrice = float() # Optional float. Use with STOP/STOP_MARKET or TAKE_PROFIT/TAKE_PROFIT_MARKET orders
-eClosePosition = str("").upper() # Optional string. true or false; Close-All, use with STOP_MARKET or TAKE_PROFIT_MARKET.
-eActivationPrice = float() # Optional float. Use with TRAILING_STOP_MARKET orders, default is the latest price
-eCallbackRate = float() # Optional float. Use with TRAILING_STOP_MARKET orders, min 0.1, max 5 where 1 for 1%.
-eWorkingType = str("CONTRACT_PRICE").upper()
-ePriceProtect = str("FALSE").upper()
-eNewOrderRespType = str("ACK").upper()
-eRecvWindow = int(5000)
-webhookData = str()
+# eSymbol = str("").upper() # BTCUSDT, BNBUSDT, ETHUSDT
+# eSide = str("").upper() # BUY, SELL
+# eType = str("MARKET").upper() # LIMIT, MARKET, STOP, STOP_MARKET, TAKE_PROFIT
+# ePositionSide = str("BOTH").upper() # BOTH, LONG, SHORT
+# eTimeInForce = str("GTC").upper()
+# eQuantity = float()
+# eReduceOnly = str("").upper()
+# ePrice = float()
+# eNewClientOrderId = "byTradingTool" # Optional string. An unique ID among open orders. Automatically generated if not sent.
+# eStopPrice = float() # Optional float. Use with STOP/STOP_MARKET or TAKE_PROFIT/TAKE_PROFIT_MARKET orders
+# eClosePosition = str("").upper() # Optional string. true or false; Close-All, use with STOP_MARKET or TAKE_PROFIT_MARKET.
+# eActivationPrice = float() # Optional float. Use with TRAILING_STOP_MARKET orders, default is the latest price
+# eCallbackRate = float() # Optional float. Use with TRAILING_STOP_MARKET orders, min 0.1, max 5 where 1 for 1%.
+# eWorkingType = str("CONTRACT_PRICE").upper()
+# ePriceProtect = str("FALSE").upper()
+# eNewOrderRespType = str("ACK").upper()
+# eRecvWindow = int(5000)
+# webhookData = str()
 
 # Homepage
 @app.route('/')
@@ -48,7 +48,13 @@ def webhook():
     eType = webhookData["eType"].upper()
     ePositionSide = webhookData["ePositionSide"].upper()
     eTimeInForce = webhookData["eTimeInForce"].upper()
-    eQuantity = webhookData["eQuantity"]
+    eQuantity = float(webhookData["eQuantity"].split()[0])
+    ePrice = float(webhookData["ePrice"])
+    pembagi = float(ePrice / eQuantity)
+    lot = float(1.0/pembagi)
+    lot = str(lot)
+    lot = lot[0:4]
+    
     eNewClientOrderId = webhookData["eNewClientOrderId"]
     print(eSymbol)
     print(eSide)
@@ -56,10 +62,12 @@ def webhook():
     print(ePositionSide)
     print(eTimeInForce)
     print(eQuantity)
+    print(pembagi)
+    print(f"Size = {lot}")
     print(eNewClientOrderId)
 
     um_futures_client = UMFutures(key=config.testnet.key, secret=config.testnet.secret, base_url=config.testnet.url)
-    newOrder = um_futures_client.new_order(symbol=eSymbol, side=eSide, type=eType, positionSide=ePositionSide, quantity=eQuantity, newClientOrderId= eNewClientOrderId)
+    newOrder = um_futures_client.new_order(symbol=eSymbol, side=eSide, type=eType, positionSide=ePositionSide, quantity=lot, newClientOrderId= eNewClientOrderId)
 
     nowtime = datetime.now()
     d = datetime.fromisoformat(str(nowtime))
